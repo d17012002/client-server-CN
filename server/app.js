cb604929a9d686c9da3c067da4c2ef2d7fd1758f
Anurag Kumar <2002anuragksingh@gmail.com>
2022-12-21 14:27:09 +0530

    server side

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const{ DateTime } = require('luxon');
+
+const app  = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//*********** Middleware **************************/
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+app.route("/")
+   .get((req, res) => {
+        let d = DateTime.local();
+
+        let date = d.toLocaleString(DateTime.DATE_FULL);
+        let time =  d.toLocaleString(DateTime.TIME_24_WITH_LONG_OFFSET);
+
+        const obj = {
+            DATE : date,
+            TIME : time
+        }
+        res.send(obj);
+   });
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server running at 🚀: http://localhost:3000/");
+});
